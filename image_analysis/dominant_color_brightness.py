5f8c62aad77784a64f507a563774b347802b6337
Benjy3141 <lemkebe2539@gmail.com>
2024-07-20 16:21:45 +0200

    Update dominant_color_brightness.py
    Combine dominant color, brightness and saturation, contrast, sharpness

--- image_analysis/dominant_color_brightness.py
+++ image_analysis/dominant_color_brightness.py
@@ -19,12 +19,28 @@ def unique_count_app(a):
     return colors[count.argmax()]
 
 
+print("dominant_color: "+str(unique_count_app(image)))
+
 #average brightness of an image
 def brightness(img):
     imghsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     return cv2.mean(imghsv)[2:3][0]
 
-#print(brightness(image))
+print("brightness: "+str(brightness(image)))
 
 
+img_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+contrast = img_grey.std()
+
+img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+saturation = img_hsv[:, :, 1].mean()
+fm = cv2.Laplacian(img_grey, cv2.CV_64F).var()
+print("contrast: "+str(contrast))
+print("saturation: "+str(saturation))
+print("Sharpness: "+str(fm))
+cv2.imshow('image',image)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
